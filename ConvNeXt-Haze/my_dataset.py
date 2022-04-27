7324cf951ee291d3622cc4b67987453c34cd92de
ChenZang <93693390+ChenZangwa@users.noreply.github.com>
2022-04-27 18:30:45 +0800

    Add files via upload

---
+++ ConvNeXt-Haze/my_dataset.py
@@ -0,0 +1,83 @@
+from PIL import Image, ImageFilter
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+import cv2
+
+class Cutblur(object):
+    def __init__(self):
+        self.name = 'CutGaussianBlur'
+
+    @staticmethod
+    def __call__(img, prob=0.95):
+
+        im2 = img.copy()
+        im2 = im2.filter(ImageFilter.GaussianBlur(radius=5))
+
+        if np.random.rand(1) >= prob:
+            return img
+
+        cut_ratio = np.random.rand()
+        # cut_ratio = 0.5
+        def get_params(img):
+            if type(img) == np.ndarray:
+                img_h, img_w, img_c = img.shape
+            else:
+                img_h, img_w = img.size
+                img_c = len(img.getbands())
+
+            return img_h, img_w
+        h, w = get_params(img)
+        ch, cw = np.int32(h * cut_ratio), np.int32(w * cut_ratio)
+        # print(ch,cw,h,w)
+        cy = np.random.randint(0, h - ch + 1)
+        cx = np.random.randint(0, w - cw + 1)
+
+        # apply CutBlur to inside or outside
+        im2 = cv2.cvtColor(np.array(im2), cv2.COLOR_RGB2BGR)
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        # if np.random.random() > 0.5:
+        #     im2[cy:cy + ch, cx:cx + cw] = img[cy:cy + ch, cx:cx + cw]
+        # else:
+        im2_aug = img.copy()
+        im2_aug[cy:cy + ch, cx:cx + cw] = im2[cy:cy + ch, cx:cx + cw]
+        im2 = im2_aug
+        im2 = Image.fromarray(cv2.cvtColor(im2, cv2.COLOR_BGR2RGB))
+        return im2
+
+
+class MyDataSet(Dataset):
+    """自定义数据集"""
+
+    def __init__(self, images_path: list, images_class: list, transform=None):
+        self.images_path = images_path
+        self.images_class = images_class
+        self.transform = transform
+        self.cutblur = Cutblur()
+
+    def __len__(self):
+        return len(self.images_path)
+
+    def __getitem__(self, item):
+        img = Image.open(self.images_path[item])
+        # RGB为彩色图片，L为灰度图片
+        img = img.convert('RGB')
+        if img.mode != 'RGB':
+            raise ValueError("image: {} isn't RGB mode.".format(self.images_path[item]))
+        label = self.images_class[item]
+        img = self.cutblur(img)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, label
+
+    @staticmethod
+    def collate_fn(batch):
+        # 官方实现的default_collate可以参考
+        # https://github.com/pytorch/pytorch/blob/67b7e751e6b5931a9f45274653f4f653a4e6cdf6/torch/utils/data/_utils/collate.py
+        images, labels= tuple(zip(*batch))
+
+        images = torch.stack(images, dim=0)
+        labels = torch.as_tensor(labels)
+        return images, labels
---
+++ ConvNeXt-Haze/utils.py
@@ -0,0 +1,496 @@
+import os
+import sys
+import json
+import pickle
+import random
+import math
+import cv2
+import torch
+from prettytable import PrettyTable
+from sklearn.metrics import precision_recall_fscore_support
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+from torch import nn
+from torch.nn import functional as F
+
+
+class ConfusionMatrix(object):
+    def __init__(self, num_classes: int, labels: list):
+        self.matrix = np.zeros((num_classes, num_classes))  # 初始化混淆矩阵，元素都为0
+        self.num_classes = num_classes  # 类别数量，本例数据集类别为5
+        self.labels = labels  # 类别标签
+
+    def update(self, preds, labels):
+        for p, t in zip(preds, labels):  # pred为预测结果，labels为真实标签
+            self.matrix[p, t] += 1  # 根据预测结果和真实标签的值统计数量，在混淆矩阵相应位置+1
+
+    def summary(self):  # 计算指标函数
+        # calculate accuracy
+        sum_TP = 0
+        n = np.sum(self.matrix)
+        for i in range(self.num_classes):
+            sum_TP += self.matrix[i, i]  # 混淆矩阵对角线的元素之和，也就是分类正确的数量
+        acc = sum_TP / n  # 总体准确率
+        print("the model accuracy is ", acc)
+
+        # kappa
+        sum_po = 0
+        sum_pe = 0
+        for i in range(len(self.matrix[0])):
+            sum_po += self.matrix[i][i]
+            row = np.sum(self.matrix[i, :])
+            col = np.sum(self.matrix[:, i])
+            sum_pe += row * col
+        po = sum_po / n
+        pe = sum_pe / (n * n)
+        # print(po, pe)
+        kappa = round((po - pe) / (1 - pe), 3)
+        # print("the model kappa is ", kappa)
+
+        # precision, recall, specificity
+        table = PrettyTable()  # 创建一个表格
+        table.field_names = ["", "Precision", "Recall", "Specificity"]
+        for i in range(self.num_classes):  # 精确度、召回率、特异度的计算
+            TP = self.matrix[i, i]
+            FP = np.sum(self.matrix[i, :]) - TP
+            FN = np.sum(self.matrix[:, i]) - TP
+            TN = np.sum(self.matrix) - TP - FP - FN
+
+            Precision = round(TP / (TP + FP), 3) if TP + FP != 0 else 0.
+            Recall = round(TP / (TP + FN), 3) if TP + FN != 0 else 0.  # 每一类准确度
+            Specificity = round(TN / (TN + FP), 3) if TN + FP != 0 else 0.
+
+            table.add_row([self.labels[i], Precision, Recall, Specificity])
+        print(table)
+        return str(acc)
+
+    def plot(self):  # 绘制混淆矩阵
+        matrix = self.matrix
+        print(matrix)
+        plt.imshow(matrix, cmap=plt.cm.Blues)
+
+        # 设置x轴坐标label
+        plt.xticks(range(self.num_classes), self.labels, rotation=45)
+        # 设置y轴坐标label
+        plt.yticks(range(self.num_classes), self.labels)
+        # 显示colorbar
+        plt.colorbar()
+        plt.xlabel('True Labels')
+        plt.ylabel('Predicted Labels')
+        plt.title('Confusion matrix (acc=' + self.summary() + ')')
+
+        # 在图中标注数量/概率信息
+        thresh = matrix.max() / 2
+        for x in range(self.num_classes):
+            for y in range(self.num_classes):
+                # 注意这里的matrix[y, x]不是matrix[x, y]
+                info = int(matrix[y, x])
+                plt.text(x, y, info,
+                         verticalalignment='center',
+                         horizontalalignment='center',
+                         color="white" if info > thresh else "black")
+        plt.tight_layout()
+        plt.show()
+
+
+
+
+def read_split_data(root: str, val_rate: float = 0.3):
+    random.seed(0)  # 保证随机结果可复现
+    assert os.path.exists(root), "dataset root: {} does not exist.".format(root)
+
+    # 遍历文件夹，一个文件夹对应一个类别
+    flower_class = [cla for cla in os.listdir(root) if os.path.isdir(os.path.join(root, cla))]
+    # 排序，保证顺序一致
+    flower_class.sort()
+    # 生成类别名称以及对应的数字索引
+    class_indices = dict((k, v) for v, k in enumerate(flower_class))
+    json_str = json.dumps(dict((val, key) for key, val in class_indices.items()), indent=4)
+    with open('class_indices.json', 'w') as json_file:
+        json_file.write(json_str)
+
+    train_images_path = []  # 存储训练集的所有图片路径
+    train_images_label = []  # 存储训练集图片对应索引信息
+    val_images_path = []  # 存储验证集的所有图片路径
+    val_images_label = []  # 存储验证集图片对应索引信息
+    every_class_num = []  # 存储每个类别的样本总数
+    supported = [".jpg", ".JPG", ".png", ".PNG"]  # 支持的文件后缀类型
+    # 遍历每个文件夹下的文件
+    for cla in flower_class:
+        cla_path = os.path.join(root, cla)
+        # 遍历获取supported支持的所有文件路径
+        images = [os.path.join(root, cla, i) for i in os.listdir(cla_path)
+                  if os.path.splitext(i)[-1] in supported]
+        # 获取该类别对应的索引
+        image_class = class_indices[cla]
+        # 记录该类别的样本数量
+        every_class_num.append(len(images))
+        # 按比例随机采样验证样本
+        val_path = random.sample(images, k=int(len(images) * val_rate))
+
+        for img_path in images:
+            if img_path in val_path:  # 如果该路径在采样的验证集样本中则存入验证集
+                val_images_path.append(img_path)
+                val_images_label.append(image_class)
+            else:  # 否则存入训练集
+                train_images_path.append(img_path)
+                train_images_label.append(image_class)
+
+    print("{} images were found in the dataset.".format(sum(every_class_num)))
+    print("{} images for training.".format(len(train_images_path)))
+    print("{} images for validation.".format(len(val_images_path)))
+    assert len(train_images_path) > 0, "not find data for train."
+    assert len(val_images_path) > 0, "not find data for eval"
+
+    plot_image = False
+    if plot_image:
+        # 绘制每种类别个数柱状图
+        plt.bar(range(len(flower_class)), every_class_num, align='center')
+        # 将横坐标0,1,2,3,4替换为相应的类别名称
+        plt.xticks(range(len(flower_class)), flower_class)
+        # 在柱状图上添加数值标签
+        for i, v in enumerate(every_class_num):
+            plt.text(x=i, y=v + 5, s=str(v), ha='center')
+        # 设置x坐标
+        plt.xlabel('image class')
+        # 设置y坐标
+        plt.ylabel('number of images')
+        # 设置柱状图的标题
+        plt.title('flower class distribution')
+        plt.show()
+
+    return train_images_path, train_images_label, val_images_path, val_images_label
+
+
+def plot_data_loader_image(data_loader):
+    batch_size = data_loader.batch_size
+    plot_num = min(batch_size, 4)
+
+    json_path = './class_indices.json'
+    assert os.path.exists(json_path), json_path + " does not exist."
+    json_file = open(json_path, 'r')
+    class_indices = json.load(json_file)
+
+    for data in data_loader:
+        images, labels = data
+        for i in range(plot_num):
+            # [C, H, W] -> [H, W, C]
+            img = images[i].numpy().transpose(1, 2, 0)
+            # 反Normalize操作
+            img = (img * [0.229, 0.224, 0.225] + [0.485, 0.456, 0.406]) * 255
+            label = labels[i].item()
+            plt.subplot(1, plot_num, i+1)
+            plt.xlabel(class_indices[str(label)])
+            plt.xticks([])  # 去掉x轴的刻度
+            plt.yticks([])  # 去掉y轴的刻度
+            plt.imshow(img.astype('uint8'))
+        plt.show()
+
+
+def write_pickle(list_info: list, file_name: str):
+    with open(file_name, 'wb') as f:
+        pickle.dump(list_info, f)
+
+
+def read_pickle(file_name: str) -> list:
+    with open(file_name, 'rb') as f:
+        info_list = pickle.load(f)
+        return info_list
+
+
+class FocalLossWithSmoothing(nn.Module):
+    def __init__(
+            self,
+            num_classes: int,
+            gamma: int = 1,
+            lb_smooth: float = 0.15,
+            size_average: bool = True,
+            ignore_index: int = None,
+            alpha: float = None):
+        """
+        :param gamma:
+        :param lb_smooth:
+        :param ignore_index:
+        :param size_average:
+        :param alpha:
+        """
+        super(FocalLossWithSmoothing, self).__init__()
+        self._num_classes = num_classes
+        self._gamma = gamma
+        self._lb_smooth = lb_smooth
+        self._size_average = size_average
+        self._ignore_index = ignore_index
+        self._log_softmax = nn.LogSoftmax(dim=1)
+        self._alpha = alpha
+
+        if self._num_classes <= 1:
+            raise ValueError('The number of classes must be 2 or higher')
+        if self._gamma < 0:
+            raise ValueError('Gamma must be 0 or higher')
+        if self._alpha is not None:
+            if self._alpha <= 0 or self._alpha >= 1:
+                raise ValueError('Alpha must be 0 <= alpha <= 1')
+
+    def forward(self, logits, label):
+        """
+        :param logits: (batch_size, class, height, width)
+        :param label:
+        :return:
+        """
+        logits = logits.float()
+        difficulty_level = self._estimate_difficulty_level(logits, label)
+
+        with torch.no_grad():
+            label = label.clone().detach()
+            if self._ignore_index is not None:
+                ignore = label.eq(self._ignore_index)
+                label[ignore] = 0
+            lb_pos, lb_neg = 1. - self._lb_smooth, self._lb_smooth / (self._num_classes - 1)
+            lb_one_hot = torch.empty_like(logits).fill_(
+                lb_neg).scatter_(1, label.unsqueeze(1), lb_pos).detach()
+        logs = self._log_softmax(logits)
+        loss = -torch.sum(difficulty_level * logs * lb_one_hot, dim=1)
+        if self._ignore_index is not None:
+            loss[ignore] = 0
+        return loss.mean()
+
+    def _estimate_difficulty_level(self, logits, label):
+        """
+        :param logits:
+        :param label:
+        :return:
+        """
+        one_hot_key = torch.nn.functional.one_hot(label, num_classes=self._num_classes)
+        if len(one_hot_key.shape) == 4:
+            one_hot_key = one_hot_key.permute(0, 3, 1, 2)
+        if one_hot_key.device != logits.device:
+            one_hot_key = one_hot_key.to(logits.device)
+        pt = one_hot_key * F.softmax(logits)
+        difficulty_level = torch.pow(1 - pt, self._gamma)
+        return difficulty_level
+
+
+def train_one_epoch(model, optimizer, data_loader, device, epoch, lr_scheduler):
+    model.train()
+    loss_function = FocalLossWithSmoothing(num_classes=5)
+    accu_loss = torch.zeros(1).to(device)  # 累计损失
+    accu_num = torch.zeros(1).to(device)   # 累计预测正确的样本数
+    optimizer.zero_grad()
+
+
+    sample_num = 0
+    data_loader = tqdm(data_loader, file=sys.stdout)
+    for step, data in enumerate(data_loader):
+        images, labels  = data
+
+        sample_num += images.shape[0]
+
+        pred = model(images.to(device))
+        pred_classes = torch.max(pred, dim=1)[1]
+        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+        loss = loss_function(pred, labels.to(device))
+
+        loss.backward()
+        accu_loss += loss.detach()
+
+        data_loader.desc = "[train epoch {}] loss: {:.3f}, acc: {:.3f}, lr: {:.5f}".format(
+            epoch,
+            accu_loss.item() / (step + 1),
+            accu_num.item() / sample_num,
+            optimizer.param_groups[0]["lr"]
+        )
+
+        if not torch.isfinite(loss):
+            print('WARNING: non-finite loss, ending training ', loss)
+            sys.exit(1)
+
+        optimizer.step()
+        optimizer.zero_grad()
+        # update lr
+        lr_scheduler.step()
+
+    return accu_loss.item() / (step + 1), accu_num.item() / sample_num
+
+
+class ConfusionMatrix(object):
+
+    def __init__(self, num_classes: int, labels: list):
+        self.matrix = np.zeros((num_classes, num_classes))  # 初始化混淆矩阵，元素都为0
+        self.num_classes = num_classes  # 类别数量，本例数据集类别为5
+        self.labels = labels  # 类别标签
+
+    def update(self, preds, labels):
+        for p, t in zip(preds, labels):  # pred为预测结果，labels为真实标签
+            self.matrix[p, t] += 1  # 根据预测结果和真实标签的值统计数量，在混淆矩阵相应位置+1
+
+    def summary(self):  # 计算指标函数
+        # calculate accuracy
+        sum_TP = 0
+        n = np.sum(self.matrix)
+        for i in range(self.num_classes):
+            sum_TP += self.matrix[i, i]  # 混淆矩阵对角线的元素之和，也就是分类正确的数量
+        acc = sum_TP / n  # 总体准确率
+        print("the model accuracy is ", acc)
+
+        # kappa
+        sum_po = 0
+        sum_pe = 0
+        for i in range(len(self.matrix[0])):
+            sum_po += self.matrix[i][i]
+            row = np.sum(self.matrix[i, :])
+            col = np.sum(self.matrix[:, i])
+            sum_pe += row * col
+        po = sum_po / n
+        pe = sum_pe / (n * n)
+        # print(po, pe)
+        kappa = round((po - pe) / (1 - pe), 3)
+        # print("the model kappa is ", kappa)
+
+        # precision, recall, specificity
+        table = PrettyTable()  # 创建一个表格
+        table.field_names = ["", "Precision", "Recall", "Specificity"]
+        for i in range(self.num_classes):  # 精确度、召回率、特异度的计算
+            TP = self.matrix[i, i]
+            FP = np.sum(self.matrix[i, :]) - TP
+            FN = np.sum(self.matrix[:, i]) - TP
+            TN = np.sum(self.matrix) - TP - FP - FN
+
+            Precision = round(TP / (TP + FP), 3) if TP + FP != 0 else 0.
+            Recall = round(TP / (TP + FN), 3) if TP + FN != 0 else 0.  # 每一类准确度
+            Specificity = round(TN / (TN + FP), 3) if TN + FP != 0 else 0.
+
+            table.add_row([self.labels[i], Precision, Recall, Specificity])
+        print(table)
+        return str(acc)
+
+    def plot(self):  # 绘制混淆矩阵
+        matrix = self.matrix
+        print(matrix)
+        plt.imshow(matrix, cmap=plt.cm.Blues)
+
+        # 设置x轴坐标label
+        plt.xticks(range(self.num_classes), self.labels, rotation=45)
+        # 设置y轴坐标label
+        plt.yticks(range(self.num_classes), self.labels)
+        # 显示colorbar
+        plt.colorbar()
+        plt.xlabel('True Labels')
+        plt.ylabel('Predicted Labels')
+        plt.title('Confusion matrix (acc=' + self.summary() + ')')
+
+        # 在图中标注数量/概率信息
+        thresh = matrix.max() / 2
+        for x in range(self.num_classes):
+            for y in range(self.num_classes):
+                # 注意这里的matrix[y, x]不是matrix[x, y]
+                info = int(matrix[y, x])
+                plt.text(x, y, info,
+                         verticalalignment='center',
+                         horizontalalignment='center',
+                         color="white" if info > thresh else "black")
+        plt.tight_layout()
+        plt.show()
+
+
+@torch.no_grad()
+def evaluate(model, data_loader, device, epoch):
+
+
+    model.eval()
+    true_label_list = []
+    pred_label_list = []
+
+    loss_function = FocalLossWithSmoothing(num_classes=5)
+    accu_num = torch.zeros(1).to(device)   # 累计预测正确的样本数
+    accu_loss = torch.zeros(1).to(device)  # 累计损失
+    lab = ['Fog-free','fog','big fog','thick fog','strong fog']
+    confusion = ConfusionMatrix(5, labels=lab)
+
+    sample_num = 0
+    data_loader = tqdm(data_loader, file=sys.stdout)
+    for step, data in enumerate(data_loader):
+        images, labels  = data
+        sample_num += images.shape[0]
+
+        pred = model(images.to(device))
+
+
+        pred_classes = torch.max(pred, dim=1)[1]
+        true_label_list.append(labels.cpu().detach().numpy())
+        pred_label_list.append(pred_classes.cpu().detach().numpy())
+
+        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+
+        loss = loss_function(pred, labels.to(device))
+        accu_loss += loss
+
+        data_loader.desc = "[valid epoch {}] loss: {:.3f}, acc: {:.3f}".format(
+            epoch,
+            accu_loss.item() / (step + 1),
+            accu_num.item() / sample_num
+        )
+        confusion.update(pred_classes.cpu().numpy(), labels.cpu().numpy())
+
+    if 0.94 <= (accu_num.item() / sample_num):
+        confusion.plot()
+    # confusion.summary()
+
+    y_true = np.concatenate(true_label_list)
+    y_pred = np.concatenate(pred_label_list)
+    precision, recall, f1 = precision_recall_fscore_support(y_true, y_pred, average='macro')[:-1]
+    return accu_loss.item() / (step + 1), accu_num.item() / sample_num ,precision, recall, f1
+
+
+def create_lr_scheduler(optimizer,
+                        num_step: int,
+                        epochs: int,
+                        warmup=True,
+                        warmup_epochs=1,
+                        warmup_factor=1e-3,
+                        end_factor=1e-6):
+    assert num_step > 0 and epochs > 0
+    if warmup is False:
+        warmup_epochs = 0
+
+    def f(x):
+        """
+        根据step数返回一个学习率倍率因子，
+        注意在训练开始之前，pytorch会提前调用一次lr_scheduler.step()方法
+        """
+        if warmup is True and x <= (warmup_epochs * num_step):
+            alpha = float(x) / (warmup_epochs * num_step)
+            # warmup过程中lr倍率因子从warmup_factor -> 1
+            return warmup_factor * (1 - alpha) + alpha
+        else:
+            current_step = (x - warmup_epochs * num_step)
+            cosine_steps = (epochs - warmup_epochs) * num_step
+            # warmup后lr倍率因子从1 -> end_factor
+            return ((1 + math.cos(current_step * math.pi / cosine_steps)) / 2) * (1 - end_factor) + end_factor
+
+    return torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=f)
+
+
+def get_params_groups(model: torch.nn.Module, weight_decay: float = 1e-5):
+    # 记录optimize要训练的权重参数
+    parameter_group_vars = {"decay": {"params": [], "weight_decay": weight_decay},
+                            "no_decay": {"params": [], "weight_decay": 0.}}
+
+    # 记录对应的权重名称
+    parameter_group_names = {"decay": {"params": [], "weight_decay": weight_decay},
+                             "no_decay": {"params": [], "weight_decay": 0.}}
+
+    for name, param in model.named_parameters():
+        if not param.requires_grad:
+            continue  # frozen weights
+
+        if len(param.shape) == 1 or name.endswith(".bias"):
+            group_name = "no_decay"
+        else:
+            group_name = "decay"
+
+        parameter_group_vars[group_name]["params"].append(param)
+        parameter_group_names[group_name]["params"].append(name)
+
+    print("Param groups = %s" % json.dumps(parameter_group_names, indent=2))
+    return list(parameter_group_vars.values())
